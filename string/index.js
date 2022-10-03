415f025dd34f8ceac9b2fcbd332097ff73f34524
SouDev01 <tbgt35@hotmail.com>
2022-10-03 09:29:45 -0300

    ...

--- string/index.js
+++ string/index.js
@@ -7,8 +7,16 @@ let texto = 'Mergulhando nas profudenzas';
 //console.log(texto.split('.')) //Retorna um array de string, sendo divido pelo o '.'(podendo escolher como separar as string no arrasy pela o chart dentro dos parenteses)
 //console.log(texto.split('')[0])//Foi separada, transformada e um array de string e escolhendo a string na posição 0
 //console.log(texto.join())//Retorna um array em uma string.Separada por pattern ','
+//console.log(texto.replace('nas', 'em')) // Retorna uma string mudando o primeiro valor pelo o segundo valor
 
 let texto1 = 'Sofia Manzano'
 
-console.log(texto1.substring(0,5) + texto1.indexOf('f') + texto1.indexOf('o'))
+//console.log(texto1.substring(0,5) + texto1.indexOf('f') + texto1.indexOf('o'))
 
+let palavra = 'sofia';
+let palavraNova = ''
+
+for(i=palavra.length-1; i >= 0; i--){
+palavraNova += palavra[i]
+}
+console.log(palavraNova)
