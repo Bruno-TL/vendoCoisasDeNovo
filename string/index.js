063909e07861b79e00d62fc68de6d29a5c746074
SouDev01 <tbgt35@hotmail.com>
2022-10-03 08:33:46 -0300

    Criando Arquivos e fazendo um exemplo

---
+++ string/index.js
@@ -0,0 +1,14 @@
+let texto = 'Mergulhando nas profudenzas';
+
+//console.log(texto.indexOf('o'))//Sempre retorna um numero.
+//console.log(texto.toUpperCase()) //Retorna um string com letras maiuscula.
+//console.log(texto.substring(3,9)) //Retorna uma string.(posição incical, posição final -1)
+//console.log(texto.concat('rio'))//COncatenando
+//console.log(texto.split('.')) //Retorna um array de string, sendo divido pelo o '.'(podendo escolher como separar as string no arrasy pela o chart dentro dos parenteses)
+//console.log(texto.split('')[0])//Foi separada, transformada e um array de string e escolhendo a string na posição 0
+//console.log(texto.join())//Retorna um array em uma string.Separada por pattern ','
+
+let texto1 = 'Sofia Manzano'
+
+console.log(texto1.substring(0,5) + texto1.indexOf('f') + texto1.indexOf('o'))
+
